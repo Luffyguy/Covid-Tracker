7d0f393c6c0a9409710826b200ee4eaeb944e92e
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-15 17:37:48 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Covid from "./components/covid"
+import Covid from "./components/covid";
+import "./App.css";
 
 const App = () => {
-  return <>
+  return (
+    <>
       <Covid />
-    </>;
+    </>
+  );
 };
 
 export default App;
--- src/components/covid.js
+++ src/components/covid.js
@@ -1,7 +1,50 @@
 import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import ".././App.css";
+
+const useStyles = makeStyles({
+  ul: {
+    listStyleType: "none",
+  },
+  list: {
+    margin: 30,
+    textAlign: "center",
+  },
+  large: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 10,
+    borderBlockColor: "black",
+    borderRadius: 50,
+    maxWidth: 500,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    textAlign: "center",
+    height: 100,
+    padding: "0 30px",
+  },
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 10,
+    borderBlockColor: "black",
+    borderRadius: 50,
+    maxWidth: 500,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    textAlign: "center",
+    height: 100,
+    padding: "0 30px",
+  },
+  p: {
+    fontSize: 20,
+    fontWeight: 500,
+  },
+});
 
 const Covid = () => {
   const [data, setData] = useState([]);
+
+  const classes = useStyles();
 
   const getData = async () => {
     try {
@@ -19,50 +62,113 @@ const Covid = () => {
   }, []);
 
   return (
-    <>
-      <section>
-        <h3>Latest data</h3>
-        <h2>Covid Tracker</h2>
+    <div className="all">
+      <h1 className="title">Covid Tracker</h1>
 
-        <ul>
-          <li>
-            <div>
-              <p> Country India </p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p> Total Confirmed </p>
-              <p>{data.confirmed}</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p> Total Active </p>
-              <p>{data.active}</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p> Total Recovered </p>
-              <p>{data.recovered}</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p> Total Deaths </p>
-              <p>{data.deaths}</p>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p> Last Updated </p>
-              <p>{data.lastupdatedtime}</p>
-            </div>
-          </li>
-        </ul>
-      </section>
-    </>
+      <marquee class="marq" bgcolor="darkgreen" direction="left" loop="">
+        <h3 className="subtitle">📍 Latest Data 📍</h3>
+      </marquee>
+      <div className="container">
+        <section>
+          <ul className={classes.ul}>
+            <li className={classes.list}>
+              <Card className={classes.large}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Country : India
+                  </Typography>
+                </CardContent>
+              </Card>
+            </li>
+            <li className={classes.list}>
+              <Card className={classes.root}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Total Confirmed
+                  </Typography>
+                  <Typography
+                    className={classes.p}
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {data.confirmed}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </li>
+            <li className={classes.list}>
+              <Card className={classes.root}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Total Active
+                  </Typography>
+                  <Typography
+                    className={classes.p}
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {data.active}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </li>
+            <li className={classes.list}>
+              <Card className={classes.root}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Total Recovered
+                  </Typography>
+                  <Typography
+                    className={classes.p}
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {data.recovered}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </li>
+            <li className={classes.list}>
+              <Card className={classes.root}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Total Deaths
+                  </Typography>
+                  <Typography
+                    className={classes.p}
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {data.deaths}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </li>
+            <li className={classes.list}>
+              <Card className={classes.root}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Last Updated
+                  </Typography>
+                  <Typography
+                    className={classes.p}
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {data.lastupdatedtime}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
   );
 };
 
