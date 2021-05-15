9ad5e2eb64bb8cc6c998894634ff50ef61f0b7ed
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-15 14:57:39 +0530

    Displayed Fetched Data

--- src/components/covid.js
+++ src/components/covid.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Covid = () => {
+  const [data, setData] = useState([]);
+
   const getData = async () => {
     try {
       const res = await fetch("https://api.covid19india.org/data.json");
-      console.log(res);
+      const covidData = await res.json();
+      console.log(covidData.statewise[0]);
+      setData(covidData.statewise[0]);
     } catch (error) {
       console.log(error);
     }
@@ -19,6 +23,44 @@ const Covid = () => {
       <section>
         <h3>Latest data</h3>
         <h2>Covid Tracker</h2>
+
+        <ul>
+          <li>
+            <div>
+              <p> Country India </p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p> Total Confirmed </p>
+              <p>{data.confirmed}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p> Total Active </p>
+              <p>{data.active}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p> Total Recovered </p>
+              <p>{data.recovered}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p> Total Deaths </p>
+              <p>{data.deaths}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <p> Last Updated </p>
+              <p>{data.lastupdatedtime}</p>
+            </div>
+          </li>
+        </ul>
       </section>
     </>
   );
